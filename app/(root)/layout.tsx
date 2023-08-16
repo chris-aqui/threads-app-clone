9d27b3da0ce64441e472ae293d382a5f78d0a2d5
Christine <2739361+christine-aqui@users.noreply.github.com>
2023-08-16 19:59:45 -0400

    wip on layout for root

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,6 +2,10 @@ import '../globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { ClerkProvider } from "@clerk/nextjs"
+import Topbar from '@/components/shared/Topbar'
+import Bottombar from '@/components/shared/Bottombar'
+import LeftSidebar from '@/components/shared/LeftSidebar'
+import RightSidebar from '@/components/shared/RightSidebar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,8 +22,21 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <Topbar />
+          <main>
+            <LeftSidebar />
+            <section className='main-container'>
+              <div className='w-full max-w-4xl'>
+                {children}
+              </div>
+            </section>
+            {/* <RightSidebar /> */}
+          </main>
+          <Bottombar />
+        </body>
       </html>
     </ClerkProvider>
   )
 }
+
