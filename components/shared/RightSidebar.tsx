9e6b68e15afc7d893fe5e7ac19d0455d4a416ff1
Christine <2739361+christine-aqui@users.noreply.github.com>
2023-08-17 00:01:38 -0400

    wip commit

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -4,17 +4,18 @@ import UserCard from "../cards/UserCard";
 
 import { fetchCommunities } from "@/lib/actions/community.actions";
 import { fetchUsers } from "@/lib/actions/user.actions";
+import { Key } from "react";
 
 async function RightSidebar() {
 	const user = await currentUser();
 	if (!user) return null;
 
-	const similarMinds = await fetchUsers({
-		userId: user.id,
-		pageSize: 4,
-	});
+	// const similarMinds = await fetchUsers({
+	// 	userId: user.id,
+	// 	pageSize: 4,
+	// });
 
-	const suggestedCOmmunities = await fetchCommunities({ pageSize: 4 });
+	// const suggestedCOmmunities = await fetchCommunities({ pageSize: 4 });
 
 	return (
 		<section className='custom-scrollbar rightsidebar'>
@@ -24,12 +25,13 @@ async function RightSidebar() {
 				</h3>
 
 				<div className='mt-7 flex w-[350px] flex-col gap-9'>
-					{suggestedCOmmunities.communities.length > 0 ? (
+					Demo suggestedCOmmunities
+					{/* {suggestedCOmmunities.communities.length > 0 ? (
 						<>
-							{suggestedCOmmunities.communities.map((community) => (
+							{suggestedCOmmunities.communities.map((community: { id: Key | null | undefined; name: string; username: string; image: string; }) => (
 								<UserCard
 									key={community.id}
-									id={community.id}
+									id={community.id ? community.id.toString() : ''}
 									name={community.name}
 									username={community.username}
 									imgUrl={community.image}
@@ -41,19 +43,20 @@ async function RightSidebar() {
 						<p className='!text-base-regular text-light-3'>
 							No communities yet
 						</p>
-					)}
+					)} */}
 				</div>
 			</div>
 
 			<div className='flex flex-1 flex-col justify-start'>
 				<h3 className='text-heading4-medium text-light-1'>Similar Minds</h3>
 				<div className='mt-7 flex w-[350px] flex-col gap-10'>
-					{similarMinds.users.length > 0 ? (
+					demo similarMinds
+					{/* {similarMinds.users.length > 0 ? (
 						<>
-							{similarMinds.users.map((person) => (
+							{similarMinds.users.map((person: { id: Key | null | undefined; name: string; username: string; image: string; }) => (
 								<UserCard
 									key={person.id}
-									id={person.id}
+									id={person.id ? person.id.toString() : ''}
 									name={person.name}
 									username={person.username}
 									imgUrl={person.image}
@@ -63,7 +66,7 @@ async function RightSidebar() {
 						</>
 					) : (
 						<p className='!text-base-regular text-light-3'>No users yet</p>
-					)}
+					)} */}
 				</div>
 			</div>
 		</section>
