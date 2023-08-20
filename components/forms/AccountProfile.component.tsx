ccb437eccc73785a62c89b9b3627932ceaed3384
Christine <2739361+christine-aqui@users.noreply.github.com>
2023-08-20 19:42:58 -0400

    added uploadthing

--- components/forms/AccountProfile.component.tsx
+++ components/forms/AccountProfile.component.tsx
@@ -106,121 +106,115 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
 	};
 
 	return (
-		<div>
-			Account Profiule
-		</div>
-	)
+		<Form {...form}>
+			<form
+				className='flex flex-col justify-start gap-10'
+				onSubmit={form.handleSubmit(onSubmit)}
+			>
+				<FormField
+					control={form.control}
+					name='profile_photo'
+					render={({ field }) => (
+						<FormItem className='flex items-center gap-4'>
+							<FormLabel className='account-form_image-label'>
+								{field.value ? (
+									<Image
+										src={field.value}
+										alt='profile_icon'
+										width={96}
+										height={96}
+										priority
+										className='rounded-full object-contain'
+									/>
+								) : (
+									<Image
+										src='/assets/profile.svg'
+										alt='profile_icon'
+										width={24}
+										height={24}
+										className='object-contain'
+									/>
+								)}
+							</FormLabel>
+							<FormControl className='flex-1 text-base-semibold text-gray-200'>
+								<Input
+									type='file'
+									accept='image/*'
+									placeholder='Add profile photo'
+									className='account-form_image-input'
+									onChange={(e) => handleImage(e, field.onChange)}
+								/>
+							</FormControl>
+						</FormItem>
+					)}
+				/>
 
-	// return (
-	// 	<Form {...form}>
-	// 		<form
-	// 			className='flex flex-col justify-start gap-10'
-	// 			onSubmit={form.handleSubmit(onSubmit)}
-	// 		>
-	// 			<FormField
-	// 				control={form.control}
-	// 				name='profile_photo'
-	// 				render={({ field }) => (
-	// 					<FormItem className='flex items-center gap-4'>
-	// 						<FormLabel className='account-form_image-label'>
-	// 							{field.value ? (
-	// 								<Image
-	// 									src={field.value}
-	// 									alt='profile_icon'
-	// 									width={96}
-	// 									height={96}
-	// 									priority
-	// 									className='rounded-full object-contain'
-	// 								/>
-	// 							) : (
-	// 								<Image
-	// 									src='/assets/profile.svg'
-	// 									alt='profile_icon'
-	// 									width={24}
-	// 									height={24}
-	// 									className='object-contain'
-	// 								/>
-	// 							)}
-	// 						</FormLabel>
-	// 						<FormControl className='flex-1 text-base-semibold text-gray-200'>
-	// 							<Input
-	// 								type='file'
-	// 								accept='image/*'
-	// 								placeholder='Add profile photo'
-	// 								className='account-form_image-input'
-	// 								onChange={(e) => handleImage(e, field.onChange)}
-	// 							/>
-	// 						</FormControl>
-	// 					</FormItem>
-	// 				)}
-	// 			/>
+				<FormField
+					control={form.control}
+					name='name'
+					render={({ field }) => (
+						<FormItem className='flex w-full flex-col gap-3'>
+							<FormLabel className='text-base-semibold text-light-2'>
+								Name
+							</FormLabel>
+							<FormControl>
+								<Input
+									type='text'
+									className='account-form_input no-focus'
+									{...field}
+								/>
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
 
-	// 			<FormField
-	// 				control={form.control}
-	// 				name='name'
-	// 				render={({ field }) => (
-	// 					<FormItem className='flex w-full flex-col gap-3'>
-	// 						<FormLabel className='text-base-semibold text-light-2'>
-	// 							Name
-	// 						</FormLabel>
-	// 						<FormControl>
-	// 							<Input
-	// 								type='text'
-	// 								className='account-form_input no-focus'
-	// 								{...field}
-	// 							/>
-	// 						</FormControl>
-	// 						<FormMessage />
-	// 					</FormItem>
-	// 				)}
-	// 			/>
+				<FormField
+					control={form.control}
+					name='username'
+					render={({ field }) => (
+						<FormItem className='flex w-full flex-col gap-3'>
+							<FormLabel className='text-base-semibold text-light-2'>
+								Username
+							</FormLabel>
+							<FormControl>
+								<Input
+									type='text'
+									className='account-form_input no-focus'
+									{...field}
+								/>
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
 
-	// 			<FormField
-	// 				control={form.control}
-	// 				name='username'
-	// 				render={({ field }) => (
-	// 					<FormItem className='flex w-full flex-col gap-3'>
-	// 						<FormLabel className='text-base-semibold text-light-2'>
-	// 							Username
-	// 						</FormLabel>
-	// 						<FormControl>
-	// 							<Input
-	// 								type='text'
-	// 								className='account-form_input no-focus'
-	// 								{...field}
-	// 							/>
-	// 						</FormControl>
-	// 						<FormMessage />
-	// 					</FormItem>
-	// 				)}
-	// 			/>
+				<FormField
+					control={form.control}
+					name='bio'
+					render={({ field }) => (
+						<FormItem className='flex w-full flex-col gap-3'>
+							<FormLabel className='text-base-semibold text-light-2'>
+								Bio
+							</FormLabel>
+							<FormControl>
+								<Textarea
+									rows={3}
+									className='account-form_input no-focus'
+									{...field}
+								/>
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
 
-	// 			<FormField
-	// 				control={form.control}
-	// 				name='bio'
-	// 				render={({ field }) => (
-	// 					<FormItem className='flex w-full flex-col gap-3'>
-	// 						<FormLabel className='text-base-semibold text-light-2'>
-	// 							Bio
-	// 						</FormLabel>
-	// 						<FormControl>
-	// 							<Textarea
-	// 								rows={10}
-	// 								className='account-form_input no-focus'
-	// 								{...field}
-	// 							/>
-	// 						</FormControl>
-	// 						<FormMessage />
-	// 					</FormItem>
-	// 				)}
-	// 			/>
-
-	// 			<Button type='submit' className='bg-primary-500'>
-	// 				{btnTitle}
-	// 			</Button>
-	// 		</form>
-	// 	</Form>
-	// );
+				<Button type='submit' className='bg-primary-500'>
+					{btnTitle}
+				</Button>
+			</form>
+		</Form>
+	);
 };
 
 export default AccountProfile;
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -3,23 +3,24 @@
 import { FilterQuery, SortOrder } from "mongoose";
 import { revalidatePath } from "next/cache";
 
-import Community from "../models/community.model";
-import Thread from "../models/thread.model";
-import User from "../models/user.model";
+// import Community from "../models/community.model";
+// import Thread from "../models/thread.model";
+// import User from "../models/user.model";
 
-import { connectToDB } from "../mongoose";
+// import { connectToDB } from "../mongoose";
 
 export async function fetchUser(userId: string) {
-	try {
-		connectToDB();
+	console.log('hello from fetchUser')
+	// try {
+	// 	connectToDB();
 
-		return await User.findOne({ id: userId }).populate({
-			path: "communities",
-			model: Community,
-		});
-	} catch (error: any) {
-		throw new Error(`Failed to fetch user: ${error.message}`);
-	}
+	// 	return await User.findOne({ id: userId }).populate({
+	// 		path: "communities",
+	// 		model: Community,
+	// 	});
+	// } catch (error: any) {
+	// 	throw new Error(`Failed to fetch user: ${error.message}`);
+	// }
 }
 
 interface Params {
